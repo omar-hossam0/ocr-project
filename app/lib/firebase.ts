a14a963e7bbfea865e62470bf2b05ce91e1f17eb
omar hossam <amorhossam2005@gmail.com>
2026-04-26 11:21:28 +0300

    feat: implement file upload page and add environment variable override for Firebase storage bucket

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -10,7 +10,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyC1xDkNx8IomETXn3LJvBXkBJvAohvy4xQ",
   authDomain: "ocr-project-f91fc.firebaseapp.com",
   projectId: "ocr-project-f91fc",
-  storageBucket: "ocr-project-f91fc.firebasestorage.app",
+  storageBucket:
+    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ||
+    "ocr-project-f91fc.firebasestorage.app",
   messagingSenderId: "907042072904",
   appId: "1:907042072904:web:311073ee8fbf268b4c2b7d",
   measurementId: "G-2DV4PRH9KZ",
